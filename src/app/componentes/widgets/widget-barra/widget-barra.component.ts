b24a1be5ce2f75c561c5d8fba44095b61c56f45d
Sergio Andres Cano Vasquez <sergio.cano@sofka.com.co>
2019-06-08 20:06:54 -0500

    Widget meta y barras

--- src/app/componentes/widgets/widget-barra/widget-barra.component.ts
+++ src/app/componentes/widgets/widget-barra/widget-barra.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ChartDataSets } from 'chart.js';
 
 @Component({
   selector: 'app-widget-barra',
@@ -10,9 +11,40 @@ export class WidgetBarraComponent implements OnInit {
   @Input()
   public widgets: Lista[];
 
+  public barChartData: Array<ChartDataSets[]> = [];
+  public barChartType = 'bar';
+  public barChartLegend = false;
+  public barChartOptions = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    }
+  };
+
   constructor() { }
 
   ngOnInit() {
+    for (let datosWidget of this.widgets) {
+      var data: ChartDataSets[] = [
+        {
+          data: datosWidget.listaDatosValor1,
+          backgroundColor: datosWidget.Color_Valor1
+        },
+        {
+          data: datosWidget.listaDatosValor2,
+          backgroundColor: datosWidget.Color_Valor2
+        },
+      ]
+      this.barChartData.push(data);
+      console.log(data);
+    }
+    console.log(this.widgets);
+    console.log(this.barChartData);
   }
 
 }
