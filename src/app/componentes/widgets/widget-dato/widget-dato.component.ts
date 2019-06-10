76a67932d77b9e2c072852cff73864b54a2456ca
Sergio Andres Cano Vasquez <sergio.cano@sofka.com.co>
2019-06-10 16:49:08 -0500

    Modal arreglo

--- src/app/componentes/widgets/widget-dato/widget-dato.component.ts
+++ src/app/componentes/widgets/widget-dato/widget-dato.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { windowToggle } from 'rxjs/operators';
 
 @Component({
   selector: 'app-widget-dato',
   templateUrl: './widget-dato.component.html',
-  styleUrls: ['./widget-dato.component.scss']
+  styleUrls: ['./widget-dato.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class WidgetDatoComponent implements OnInit {
 
@@ -13,15 +16,19 @@ export class WidgetDatoComponent implements OnInit {
   public itemsModal: ListaItem[];
   public tituloModal: string;
 
-  constructor() { }
+  // Ng Bootstrap modal
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
     this.itemsModal = this.widgets[0].listaItems;
     this.tituloModal = this.widgets[0].Tema_Descripcion;
   }
 
-  showWidgetDatoInModal(index: number) {
+  showWidgetDatoInModal(index: number, content) {
     this.itemsModal = this.widgets[index].listaItems;
     this.tituloModal = this.widgets[index].Tema_Descripcion;
+    this.modalService.open(content, { centered: true, size: 'lg', windowClass: 'animated fadeIn' });
   }
 }
